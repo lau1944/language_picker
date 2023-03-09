556aa9e0de8793bb70477782aaae063693dd8af7
lau1944 <a954723219@gmail.com>
2023-03-09 14:13:48 -0500

    feat: add controller method

--- lib/language_picker_dropdown_controller.dart
+++ lib/language_picker_dropdown_controller.dart
@@ -6,4 +6,9 @@ class LanguagePickerDropdownController extends ValueNotifier<Language> {
   /// @param value the pre-selected value. Note that LanguagePickerDropdown's
   /// `initialValue` takes precedence over this parameter.
   LanguagePickerDropdownController(Language value) : super(value);
+
+  void changeLang(Language lang) {
+    this.value = lang;
+    notifyListeners();
+  }
 }
